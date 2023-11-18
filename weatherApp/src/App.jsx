fc2c10c9ab5dafbcda301fd3e40faa428c53d218
RyanLama <parbattamang1234@gmail.com>
2023-11-18 21:00:58 +0545

    Initial commit

---
+++ weatherApp/src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+
+  return (
+    <>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />}/>
+    </Routes>
+   </BrowserRouter>
+
+        </>
+  )
+}
+
+export default App
