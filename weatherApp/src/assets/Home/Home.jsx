fc6f512810aa10f89f64af79d919c5fe1a00e84e
RyanLama <parbattamang1234@gmail.com>
2023-11-20 13:00:57 +0545

    ui design

--- weatherApp/src/assets/Home/Home.jsx
+++ weatherApp/src/assets/Home/Home.jsx
@@ -17,40 +17,47 @@ const Home = () => {
   const [ longitude, setLongitude ] = useState("")
   const [ latitude, setLatitude ] = useState("")
  
- const getLocation = async () => {
-       const response = await fetch(`${api.base}weather?q=${search}&appid=${api.apiKey}`)
-       const data = await response.json()
-       console.log(data)
-       setWeather(data)
 
-       const kelvin = data.main.temp
-       const celcius  = kelvin - 273.15
-       const longitude = data.coord.lat
-       const latitude = data.coord.lon
-
-      let temp = parseFloat(celcius.toFixed(2))
+  
+    const getLocation = async () => {
+      const response = await fetch(`${api.base}weather?q=${search}&appid=${api.apiKey}`)
+      const data = await response.json()
       
 
-      setTemperature(temp)
-      setKelvin(kelvin)
-      setCondition(data.weather[0].description)
-      setLongitude(longitude)
-      setLatitude(latitude)
-      
-  }
+      setWeather(data)
+
+      const kelvin = data.main.temp
+      const celcius  = kelvin - 273.15
+      const longitude = data.coord.lat
+      const latitude = data.coord.lon
+
+     let temp = parseFloat(celcius.toFixed(2))
+     
+
+     setTemperature(temp)
+     setKelvin(kelvin)
+     setCondition(data.weather[0].description)
+     setLongitude(longitude)
+     setLatitude(latitude)
+     
+ }
+  
+ 
 
 
   return (
     <>
       <div className="wrapper">
       <div className="search-container">
-    <input type="search" placeholder="Enter city name.." onChange= { (e) => setSearch(e.target.value)} />
-    <button onClick={getLocation}>Search</button>
+    <input className="search-box" type="search" placeholder="Search city" onChange= { (e) => setSearch(e.target.value)} />
+   <img src='images/searchicon.png' className="search-icon" onClick={getLocation} />
   </div>
         <p>{weather.name}</p>
-        <p>{temperature}/{kelvin}</p>
+        <p>{temperature}</p>
+        <p>{kelvin}</p>
         <p>{condition}</p>
-        <p>{longitude} || {latitude}</p>
+        <p>{longitude}</p>
+        <p> {latitude}</p>
       </div>
     </>
   )
