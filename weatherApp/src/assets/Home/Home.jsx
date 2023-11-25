0325a1cc04fcbe2ab0e2f254797f8f2a8028c498
RyanLama <parbattamang1234@gmail.com>
2023-11-25 21:23:38 +0545

    add weather data

--- weatherApp/src/assets/Home/Home.jsx
+++ weatherApp/src/assets/Home/Home.jsx
@@ -14,12 +14,18 @@ const Home = () => {
   const [ temperature, setTemperature ] = useState("")
   const [ kelvin, setKelvin ] = useState("")
   const [ condition, setCondition ] = useState("")
+  const [ locationName, setLocationName ] = useState("")
   const [ longitude, setLongitude ] = useState("")
   const [ latitude, setLatitude ] = useState("")
   const [ icon, setIcon ] = useState("")
   const [ currentTime, setCurrentTime ] = useState(new Date())
   const [ currentDate, setCurrentDate ] = useState(new Date())
   const [ searchClicked, setSearchClicked ] = useState(false)
+  const [ handleBack, setHandleBack ] = useState(false)
+  const [ feelsLike, setFeelsLike ] = useState("")
+  const [ weatherWrapper, setWeatherWrapper ] = useState(false)
+
+  const [inputEnabled, setInputEnabled] = useState(true)
  
 
   
@@ -36,11 +42,18 @@ const Home = () => {
       const latitude = data.coord.lon
       const wicon = data.weather[0].icon
       const png = ".png"
+      const degreeIcon = "°"
+      const locationData = data.name
+
+
+      const feelsLikeData = data.main.feels_like
+      const feelsLikeTemp = feelsLikeData - 273.15
 
       const baseIconUrl = "https://openweathermap.org/img/wn/"
       const finalIcon = baseIconUrl +  wicon + png
 
-     let temp = parseFloat(celcius.toFixed(2))
+     let temp = parseFloat(celcius.toFixed(0)) + degreeIcon
+     let feelsLikeCelcius = parseFloat(feelsLikeTemp.toFixed(0)) + degreeIcon
      
 
      setTemperature(temp)
@@ -48,14 +61,26 @@ const Home = () => {
      setCondition(data.weather[0].description)
      setLongitude(longitude)
      setLatitude(latitude)
-     setIcon(finalIcon)
+     setIcon(finalIcon)     
+     setLocationName(locationData)
+     setFeelsLike(feelsLikeCelcius)
 
-     
+     setWeatherWrapper(true)
  }
 
 const handleSearchClick = () => {
-  setSearchClicked(!searchClicked)
-  
+  if(inputEnabled){
+    setSearchClicked(!searchClicked)
+    setInputEnabled(false)
+    setHandleBack(true)
+  }
+ }
+
+ const handleBackClick = () => {
+  setSearchClicked(false)
+  setInputEnabled(true)
+  setHandleBack(false)
+  setWeatherWrapper(false)
  }
 
  useEffect(() => {
@@ -74,25 +99,46 @@ const handleSearchClick = () => {
   
  }, [])
 
- 
- 
-  
- 
-
 
   return (
     <>
       <div className={`wrapper${searchClicked ? 'Clicked' : ''}`}>
       <div className={`search-container${searchClicked ? 'Clicked' : ''}`}>
+
+     {handleBack && (
+      <img className='back-button' src='images/backk.png' onClick={handleBackClick} />
+     )}
+
     <input className="search-box" type="search" placeholder="Search city" onChange= { (e) => setSearch(e.target.value)} onClick={handleSearchClick} />
    <img src='images/searchicon.png' className="search-icon"  onClick={getLocation} />
   </div>
   
+  
         <p className='weather-data'>{weather.name}</p>
-        <p className='weather-data'>{temperature}</p>
+        
         <p className='weather-data'>{kelvin}</p>
-        <img className="weather-icon" src={icon} alt='Image not found' />
-        <p className='weather-data'>{condition}</p>
+
+
+
+
+        {weatherWrapper &&(
+        <div className='weather-wrapper'>
+
+        <img className='weather-icon' src={icon} alt='Image not found' />
+        <div className='location-data'>
+        <img className='location-icon' src='/images/location.svg' />
+        <p className='location-name'>{locationName}</p>
+        <p className='temperature-data'>{temperature}</p>
+        <p className='feels-like'>Feels Like {feelsLike}</p>
+
+        </div>
+
+
+
+        <p className='condition-data'>{condition}</p>
+        </div>
+        )}
+
         <p className='weather-data'>{longitude}</p>
         <p className='weather-data'> {latitude}</p>
         <p className={`time${searchClicked ? 'Clicked' : ''}`}> {currentTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit'})}</p>
